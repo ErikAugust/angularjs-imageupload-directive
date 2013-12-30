fb8a06afdf732beeae4f6c4e2755af7cdc539af6
Erik Johnson <erik@erikaugust.com>
2013-12-30 09:24:48 -0500

    Adds image rotation to directive

--- README.md
+++ README.md
@@ -23,13 +23,14 @@ The image object has the following properties:
 - url
 - dataURL
 
-### Single image with resizing
+### Single image with resizing and <strong>image rotation</strong>
 
 ```html
 <input type="file" accept="image/*" image="image2"
     resize-max-height="300"
     resize-max-width="250"
-    resize-quality="0.7" />
+    resize-quality="0.7" 
+	<strong>rotate-angle="90"</strong> />
 Original <img ng-show="image2" ng-src="{{image2.url}}" type="{{image2.file.type}}" />
 Resized <img ng-show="image2" ng-src="{{image2.resized.dataURL}}" />
 ```
@@ -50,7 +51,8 @@ The image object has the following properties:
     image="images"
     resize-max-height="300"
     resize-max-width="250"
-    resize-quality="0.7" />
+    resize-quality="0.7"
+	rotate-angle="180" />
 Originals <img ng-repeat="img in images" ng-src="{{img.url}}" type="{{img.file.type}}" />
 Resized <img ng-repeat="img in images" ng-src="{{img.resized.dataURL}}" />
 ```
@@ -72,18 +74,19 @@ See [demo.html](demo.html) for more concrete examples.
 - resize-type (default is 'image/jpg')
 - resize-max-height (default is 300)
 - resize-max-width (default is 250)
-
+- <strong>rotate-angle (default is 0)</strong>
 
 ## Features
 
 - Upload Image with FileReader
 - Resize Image via canvas
+- <strong>Rotate Image</strong>
 - Send Image Data URL (base64) to whatever you want.
 
 ## How to run the Demo?
 
 ```Shell
-git clone https://github.com/Mischi/angularjs-imageupload-directive.git
+git clone https://github.com/ErikAugust/angularjs-imageupload-directive.git
 cd angularjs-imageupload-directive
 npm install
 
@@ -107,12 +110,8 @@ Testimage: 4320x3240 4.22 MB, Resized (70% jpg): 320x270
 ## Known Issues
 
 - filesize can vary from Browser to Browser.
+- Squash image and subsampling issues in iOS
 
 
-## TODO's
-
-- Use NgModelController instead of image attribute
-- Create [bower](http://bower.io/) compatible repository (component.json, tags, etc.)
-- Match [angular-component-spec](https://github.com/PascalPrecht/angular-component-spec) when it becomes available
-- Clear image property when Form has been reset
-- Create Unit Tests
+## TODOs
+- Add Fix Squash and Subsampling functions
--- public/javascripts/imageupload.js
+++ public/javascripts/imageupload.js
@@ -21,9 +21,10 @@ angular.module('imageupload', [])
 
         var resizeImage = function (origImage, options) {
             var maxHeight = options.resizeMaxHeight || 300;
-            var maxWidth = options.resizeMaxWidth || 250;
+            var maxWidth = options.resizeMaxWidth || 300;
             var quality = options.resizeQuality || 0.7;
             var type = options.resizeType || 'image/jpg';
+			var rotateAngle = options.rotateAngle || 0;
 
             var canvas = getResizeArea();
 
@@ -46,18 +47,106 @@ angular.module('imageupload', [])
             canvas.width = width;
             canvas.height = height;
 
+			var dataURL = canvas.toDataURL(type, quality);
+
+
+			var orientation = 1;
+            orientation = rotate(orientation, rotateAngle);
+
             //draw image on canvas
             var ctx = canvas.getContext("2d");
-            ctx.drawImage(origImage, 0, 0, width, height);
+			transformCoordinate(canvas, width, height, orientation);
+			ctx.drawImage(origImage, 0, 0, width, height);
 
             // get the data from canvas as 70% jpg (or specified type).
             return canvas.toDataURL(type, quality);
         };
 
+		var rotate = function(orientation, angle) {
+            var o = {
+                // nothing
+                1: {90: 6, 180: 3, 270: 8},
+                // horizontal flip
+                2: {90: 7, 180: 4, 270: 5},
+                // 180 rotate left
+                3: {90: 8, 180: 1, 270: 6},
+                // vertical flip
+                4: {90: 5, 180: 2, 270: 7},
+                // vertical flip + 90 rotate right
+                5: {90: 2, 180: 7, 270: 4},
+                // 90 rotate right
+                6: {90: 3, 180: 8, 270: 1},
+                // horizontal flip + 90 rotate right
+                7: {90: 4, 180: 5, 270: 2},
+                // 90 rotate left
+                8: {90: 1, 180: 6, 270: 3}
+            };
+            return o[orientation][angle] ? o[orientation][angle] : orientation;
+        };
+
+		var transformCoordinate = function(canvas, width, height, orientation) {
+            switch (orientation) {
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                    canvas.width = height;
+                    canvas.height = width;
+                    break;
+                default:
+                    canvas.width = width;
+                    canvas.height = height;
+            }
+            var ctx = canvas.getContext('2d');
+            switch (orientation) {
+                case 1:
+                    // nothing
+                    break;
+                case 2:
+                    // horizontal flip
+                    ctx.translate(width, 0);
+                    ctx.scale(-1, 1);
+                    break;
+                case 3:
+                    // 180 rotate left
+                    ctx.translate(width, height);
+                    ctx.rotate(Math.PI);
+                    break;
+                case 4:
+                    // vertical flip
+                    ctx.translate(0, height);
+                    ctx.scale(1, -1);
+                    break;
+                case 5:
+                    // vertical flip + 90 rotate right
+                    ctx.rotate(0.5 * Math.PI);
+                    ctx.scale(1, -1);
+                    break;
+                case 6:
+                    // 90 rotate right
+                    ctx.rotate(0.5 * Math.PI);
+                    ctx.translate(0, -height);
+                    break;
+                case 7:
+                    // horizontal flip + 90 rotate right
+                    ctx.rotate(0.5 * Math.PI);
+                    ctx.translate(width, -height);
+                    ctx.scale(-1, 1);
+                    break;
+                case 8:
+                    // 90 rotate left
+                    ctx.rotate(-0.5 * Math.PI);
+                    ctx.translate(-width, 0);
+                    break;
+                default:
+                    break;
+            }
+        };
+
         var createImage = function(url, callback) {
-            var image = new Image();
+			var image = new Image();
             image.onload = function() {
-                callback(image);
+				callback(image);
             };
             image.src = url;
         };
@@ -74,16 +163,16 @@ angular.module('imageupload', [])
 
 
         return {
-            restrict: 'A',
+            restrict: 'EA',
             scope: {
                 image: '=',
                 resizeMaxHeight: '@?',
                 resizeMaxWidth: '@?',
                 resizeQuality: '@?',
                 resizeType: '@?',
+				rotateAngle: '=',
             },
             link: function postLink(scope, element, attrs, ctrl) {
-
                 var doResizing = function(imageResult, callback) {
                     createImage(imageResult.url, function(image) {
                         var dataURL = resizeImage(image, scope);
@@ -106,20 +195,15 @@ angular.module('imageupload', [])
                 };
 
 
-                element.bind('change', function (evt) {
-                    //when multiple always return an array of images
-                    if(attrs.multiple)
-                        scope.image = [];
-
-                    var files = evt.target.files;
-                    for(var i = 0; i < files.length; i++) {
+				var initImages = function() {
+					for(var i = 0; i < scope.files.length; i++) {
                         //create a result object for each file in files
                         var imageResult = {
-                            file: files[i],
-                            url: URL.createObjectURL(files[i])
+                            file: scope.files[i],
+                            url: URL.createObjectURL(scope.files[i])
                         };
 
-                        fileToDataURL(files[i]).then(function (dataURL) {
+                        fileToDataURL(scope.files[i]).then(function (dataURL) {
                             imageResult.dataURL = dataURL;
                         });
 
@@ -132,6 +216,30 @@ angular.module('imageupload', [])
                             applyScope(imageResult);
                         }
                     }
+				};
+
+				scope.$watch('rotateAngle', function(){ 
+					if(!scope.files) {
+						return;
+					}
+					//when multiple always return an array of images
+					if(attrs.multiple)
+                        scope.image = [];
+
+					initImages();
+
+            	});
+
+
+                element.bind('change', function (evt) {
+
+					//when multiple always return an array of images
+                    if(attrs.multiple)
+                        scope.image = [];
+
+                    scope.files = evt.target.files;
+
+					initImages();
                 });
             }
         };
